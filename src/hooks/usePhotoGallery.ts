6552143a71312db3f72931960f6019cadc2856a9
Romy Maghsoudi <romy_mag@outlook.com>
2020-04-27 13:28:59 -0400

    saving photos to fs

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -14,6 +14,7 @@ import {
 // custom hook that exposes the takePhoto method
 export function usePhotoGallery() {
   const { getPhoto } = useCamera();
+  const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
   const [photos, setPhotos] = useState<Photo[]>([]);
 
   // uses capacitor's getPhoto method which abstracts platform-specific code
@@ -21,23 +22,37 @@ export function usePhotoGallery() {
     const cameraPhoto = await getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
-      quality: 100,
+      quality: 100
     });
-    const fileName = new Date().getTime() + ".jpeg";
-    const newPhotos = [
-      {
-        filepath: fileName,
-        webviewPath: cameraPhoto.webPath,
-      },
-      ...photos,
-    ];
+  
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFileImage = await savePicture(cameraPhoto, fileName);
+    const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+  };  
+
+
+
+  const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    const savedFile = await writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+  
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    };
   };
 
   return {
     photos,
-    takePhoto,
-  };
+    takePhoto
+  }
 }
 
 // type that'll hold metadata for our Photo
