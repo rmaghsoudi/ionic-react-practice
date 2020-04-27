7f993d226568e7354f9765537f262c3d51e76a03
Romy Maghsoudi <romy_mag@outlook.com>
2020-04-27 13:01:46 -0400

    displays photos in grid

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -16,7 +16,7 @@ import {
   IonActionSheet,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
-import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { usePhotoGallery } from "../hooks/usePhotoGallery";
 import "./Tab2.css";
 
 const Tab2: React.FC = () => {
@@ -35,7 +35,15 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
